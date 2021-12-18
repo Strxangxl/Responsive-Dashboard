67c540a3e44916c10b81f27f46763cd590cd33a5
Ratan <chaddharatandeepsingh2345@gmail.com>
2021-12-18 22:25:09 +0530

    first commit

---
+++ order.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 drone',
+        productNumber: '49378',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with camera drone',
+        productNumber: '96699',
+        paymentStatus: 'Paid',
+        shipping: 'Delievered'
+    },
+    {
+        productName: 'GPS 4k drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delievered'
+    },
+    {
+        productName: 'DJI Air 2S',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lozenger drone',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delievered'
+    }
+]
